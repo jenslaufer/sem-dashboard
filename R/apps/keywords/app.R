b55062895567b0ac0a8886bcbb2ad88352ab1bb9
jenslaufer <jenslaufer@gmail.com>
2020-05-18 17:25:34 +0200

    implemented filtering

--- R/apps/keywords/app.R
+++ R/apps/keywords/app.R
@@ -1,54 +1,153 @@
 library(tidyverse)
 library(zoo)
+library(ggrepel)
 library(shiny)
 library(semtools)
+library(DT)
+library(logging)
+library(glue)
+library(shinyWidgets)
+library(shinythemes)
+library(shinycssloaders)
 
-ui <- fluidPage(titlePanel("Keyword Analysis"),
-                
-                sidebarLayout(
-                    sidebarPanel(
-                        fileInput(
-                            "file1",
-                            "Choose CSV File",
-                            multiple = TRUE,
-                            accept = c("text/csv",
-                                       "text/comma-separated-values,text/plain",
-                                       ".csv")
-                        ),
-                        sliderInput(
-                            "bins",
-                            "Number of bins:",
-                            min = 1,
-                            max = 50,
-                            value = 30
-                        )
-                    ),
-                    
-                    mainPanel(plotOutput("distPlot"))
-                ))
 
-server <- function(input, output) {
-    output$distPlot <- renderPlot({
-        req(input$file1)
-        tryCatch(
-            {
-                data <- (input$file1 %>% pull(datapath)) %>% semtools::load.keywords()
-            },
-            error = function(e) {
-                print(e)
-                stop(safeError(e))
-            }
+.slider.input <- function(name,
+                          title,
+                          .min = 0,
+                          .max = 1,
+                          .value = c(.min, .max)) {
+    sliderInput(name,
+                title,
+                min = .min,
+                max = .max,
+                value = .value)
+}
+
+
+
+.update.slider <- function(data, session, field, name = field) {
+    max <- data %>% pull(field) %>% max(na.rm = TRUE)
+    min <-
+        data %>% pull(field) %>% min(na.rm = TRUE)
+    
+    logdebug(".update.slider: min: {min}, max: {max}" %>% glue())
+    
+    updateSliderInput(
+        session,
+        name,
+        max = max,
+        min = min,
+        value = c(min, max)
+    )
+}
+
+.update.numeric.range.input <-
+    function(data, session, field, name = field) {
+        logdebug(".update.numeric.range.input...")
+        max <- data %>% pull(field) %>% max(na.rm = TRUE)
+        min <-
+            data %>% pull(field) %>% min(na.rm = TRUE)
+        
+        logdebug(".update.numeric.range.input: min: {min}, max: {max}" %>% glue())
+        
+        updateNumericRangeInput(session,
+                                name,
+                                label = "",
+                                value = c(min, max))
+    }
+
+
+
+ui <- fluidPage(
+    theme = shinytheme("cosmo"),
+    chooseSliderSkin("Modern", "DimGray"),
+    titlePanel("Keyword Analysis"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            fileInput(
+                "keywordFiles",
+                "Choose CSV File(s)",
+                multiple = TRUE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")
+            ),
+            
+            selectInput(
+                "xScale",
+                "Scaling Y",
+                list("Logarithmic" = "log10", "Linear" = "identity"),
+                selected = "Logarithmic"
+            ),
+            selectInput(
+                "yScale",
+                "Scaling Y",
+                list("Logarithmic" = "log10", "Linear" = "identity"),
+                selected = "Logarithmic"
+            ),
+            .slider.input("competition", "Competition", 0, 100),
+            .slider.input("bid", "Bid"),
+            .slider.input("avg.monthly.searches", "Average Monthy Searches"),
+            radioButtons("plotLabels",
+                         "Plot Labels",
+                         c("Yes" = T,
+                           "No" = F), selected = F),
+            .slider.input("alpha", "Alpha", .value = 1)
+            
+        ),
+        
+        mainPanel(plotOutput("keywordPlot") %>% withSpinner(type = 6))
+    )
+)
+
+server <- function(input, output, session) {
+    initial <- reactive({
+        req(input$keywordFiles)
+        data <-
+            (input$keywordFiles %>% pull(datapath)) %>%
+            semtools::load.keywords()
+        
+        
+        .update.slider(data, session, "bid")
+        .update.slider(data, session, "avg.monthly.searches")
+        
+        input$keywordFiles
+    })
+    
+    data <- reactive({
+        tryCatch({
+            data <-
+                (initial() %>% pull(datapath)) %>%
+                semtools::load.keywords()
+            
+            data %>%
+                filter(competition >= input$competition[1] &
+                           competition <= input$competition[2]) %>%
+                filter(
+                    avg.monthly.searches >= input$avg.monthly.searches[1] &
+                        avg.monthly.searches <= input$avg.monthly.searches[2]
+                ) %>%
+                filter(bid >= input$bid[1] &
+                           bid <= input$bid[2])
+            
+        },
+        error = function(e) {
+            logerror(e)
+            stop(safeError(e))
+        })
+    })
+    
+    output$keywordPlot <- renderPlot({
+        semtools::keyword.plot(
+            data(),
+            .alpha = input$alpha,
+            .x.trans = input$xScale,
+            .y.trans = input$yScale,
+            .labels = input$plotLabels
         )
-        
-        
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        hist(x,
-             breaks = bins,
-             col = 'darkgray',
-             border = 'white')
     })
 }
 
+basicConfig(level = 10)
 shinyApp(ui = ui, server = server)
